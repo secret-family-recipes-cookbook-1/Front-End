4ef62f8abf1f644e8a3e6f3fbc2d5b413c0a0664
Ken Diehl <diehlkj.99@gmail.com>
2020-02-25 23:07:57 -0500

    Initial Setup

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Secret Family Recipes</h1>
+    </div>
+  );
+}
+
+export default App;
