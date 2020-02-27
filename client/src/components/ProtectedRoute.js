deb92a1e4758ca783e9a5bcbdd1a13e15f1a28bf
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-02-27 00:11:27 -0500

    Protected Route built. Routes set up

---
+++ client/src/components/ProtectedRoute.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
+
+const ProtectedRoute = ({ component: Component, ...props }) => {
+  return (
+    <Route {...props} render={() => {
+      if (localStorage.getItem("token")) {
+        return <Component />;
+      } else {
+        return <Redirect to="/" />
+      }
+    }} />
+  )
+}
+
+export default ProtectedRoute;
