a6d1708c830e79bcac10982de06e7099fea39725
Ken Diehl <diehlkj.99@gmail.com>
2020-02-27 21:53:34 -0500

    Some changes before origin pull

--- client/src/components/RecipePage.js
+++ client/src/components/RecipePage.js
@@ -2,3 +2,5 @@
 //  If recipe is one published by the current user, additional options are listed:
 //  Option to delete recipe
 //  Option to edit recipe (Renders inline with turnary operator checking editing status)
+
+// import React from 'react';
