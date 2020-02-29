0521c060a0e1b14895f57f20f2942b9d3432ab3c
Ken Diehl <diehlkj.99@gmail.com>
2020-02-28 21:44:54 -0500

    RecipePage listing section complete, working on edit function and axios stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import RecipeContext from './contexts/RecipeContext';
 
 import Navigation from "./components/Navigation"
 import ProtectedRoute from './components/ProtectedRoute';
@@ -8,25 +9,29 @@ import Login from "./components/Login"
 import Register from "./components/Register"
 import LandingPage from "./components/LandingPage"
 import AddRecipe from "./components/AddRecipe"
-import EditRecipe from "./components/EditRecipe"
+import RecipePage from './components/RecipePage';
 
 
 function App() {
+
+  const [recipeList, setRecipeList] = useState();
+
   return (
     <Router>
-      <div className="App">
-        <h1>Secret Family Recipes</h1>
-        <Navigation />
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={Register} />
-          <ProtectedRoute path="/profile" component={UserProfile} />
-          <Route path="/add-recipe" component={AddRecipe} />
-          <Route path="/edit-recipe" component={EditRecipe} />
-        </Switch>
-
-      </div>
+      <RecipeContext.Provider value={{ recipeList, setRecipeList }} >
+        <div className="App">
+          <h1>Secret Family Recipes</h1>
+          <Navigation />
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Register} />
+            <ProtectedRoute path="/profile" component={UserProfile} />
+            <Route path="/add-recipe" component={AddRecipe} />
+            <Route path="/recipe/:id" component={RecipePage} />
+          </Switch>
+        </div>
+      </RecipeContext.Provider>
     </Router>
   );
 }
--- client/src/components/RecipePage.js
+++ client/src/components/RecipePage.js
@@ -3,4 +3,146 @@
 //  Option to delete recipe
 //  Option to edit recipe (Renders inline with turnary operator checking editing status)
 
-// import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import { useForm } from '../utils/useForm';
+import RecipeContext from '../contexts/RecipeContext';
+
+const RecipePage = props => {
+    const {recipeList, setRecipeList} = useContext(RecipeContext);  // Importing global recipe list context
+    const [localRecipe, setLocalRecipe] = useState();               // Local state slice for current recipe being viewed
+    const [editing, setEditing] = useState(false);                  // Editing toggle. Defaults to FALSE.
+
+    // {
+    //     title: '',
+    //     ingredients: [],
+    //     rid: '',
+    //     source: '',
+    //     fname: '',
+    //     lname: '',
+    //     body: '',
+    //     instructions: [],
+    //     category: '',
+    //     image: ''
+    // }
+
+    useEffect(() => {
+        axiosWithAuth()
+            .get(`/recipe/${props.match.params.id}`)
+            .then(res => setLocalRecipe(res.data))
+    }, []);
+
+    const handleEdit = () => {                                      // handleEdit manages editing and PUTting of data to the API
+
+    }
+
+    const handleDelete = () => {                                    // handleDelete manages DELETE of data from the API
+
+    }
+
+    const [values, handleChanges, handleSubmit] = useForm({
+        title: '',
+        ingredients: [],
+        source: '',
+        body: '',
+        instructions: [],
+        category: '',
+        image: ''
+    },
+        submitCallback
+    );
+
+    return (
+        <div className='recipe-page'>
+            {!editing ? (                                           // If editing is true, show edit form:
+                <form className='recipe-edit'>
+                    <section>
+                        <div>
+                            <input type='text' name='title' value={values.title} onChange={handleChanges} />
+                            <label>{`By: ${localRecipe.fname} ${localRecipe.lname} From: `}<input type='text' name='source' value={values.source} onChange={handleChanges} /></label> 
+                            <select name='category' value={values.category} onChange={handleChanges}>
+                                <option value='breakfast'>Breakfast</option>
+                                <option value='lunch'>Lunch</option>
+                                <option value='dinner'>Dinner</option>
+                                <option value='desert'>Desert</option>
+                                <option value='beverage'>Beverage</option>
+                            </select>
+                        </div>
+                        <figure>
+                            <input type='file' name='image' value={values.image} onChange={handleChanges} />
+                        </figure>
+                    </section>
+
+                    <section>
+                        <textarea name='body' value={values.body} ></textarea>
+                    </section>
+
+                    <section>
+                        <div>
+                            <h2>Ingredients</h2>
+                            <ul>                                    {/* Map over data array and create list */} 
+                                {localRecipe.ingredients.map((item) => {
+                                    return (
+                                        <li>{item}</li>
+                                    );
+                                })}
+                            </ul>
+                        </div>
+                        <div>
+                            <h2>Instructions</h2>
+                            <ol>                                    {/* Map over data array and create list */} 
+                                {localRecipe.ingredients.map((item) => {
+                                    return (
+                                        <li>{item}</li>
+                                    );
+                                })}
+                            </ol>
+                        </div>
+                    </section>
+                </form>
+            ) : (                                                   // If editing is false, show recipe listing:
+                <div className='recipe-info'>
+                    <section>
+                        <div>
+                            <h1>{localRecipe.title}</h1>
+                            <h3>By: {localRecipe.fname} {localRecipe.lname} | From: {localRecipe.source}</h3>
+                            <h4>{localRecipe.category}</h4>
+                        </div>
+                        <figure></figure>
+                    </section>
+
+                    <section>
+                    <p>{localRecipe.body}</p>
+                    </section>
+
+                    <section>
+                        <div>
+                            <h2>Ingredients</h2>
+                            <ul>                                    {/* Map over data array and create list */} 
+                                {localRecipe.ingredients.map((item) => {
+                                    return (
+                                        <li>{item}</li>
+                                    );
+                                })}
+                            </ul>
+                            
+                        </div>
+                        <div>
+                            <h2>Instructions</h2>
+                            <ol>                                    {/* Map over data array and create list */} 
+                                {localRecipe.ingredients.map((item) => {
+                                    return (
+                                        <li>{item}</li>
+                                    );
+                                })}
+                            </ol>
+                        </div>
+                    </section>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default RecipePage;
