80d1544ef24585cf4cea43c5a74cdaccc8f42ca4
Ken Diehl <diehlkj.99@gmail.com>
2020-03-02 23:11:08 -0500

    Removed Modal, removed UserProfile.js, moved recipe card to its own component, updated routes.

--- client/src/components/RecipeList.js
+++ client/src/components/RecipeList.js
@@ -3,39 +3,23 @@
 import React, { useState, useContext } from "react";
 import RecipeContext from "../contexts/RecipeContext";
 import { Link } from "react-router-dom";
+import RecipeCard from "./RecipeCard";
 
 
 const RecipeList = () => {
 	const { recipeList, setRecipeList } = useContext(RecipeContext);
 
 	console.log("MOCK DATA", recipeList);
-
-	const recipe = recipeList.map(recipe => {
-		return (
-			<Link to={`/recipe-page/${recipe.id}`} key={recipe.id}>
-				<div className='recipe__container'>
-					<div className='recipe__image__container'>
-						<img src={recipe.image} />
-					</div>
-					<div className='recipe__body'>
-						<h4 className='recipe__title'>{recipe.title}</h4>
-						<p className='recipe__source'>From {recipe.source}</p>
-						<p className='recipe__author'>
-							By {recipe.firstName} {recipe.lastName}
-						</p>
-						<p className='recipe__label__container'>
-							<span className='recipe__label'>{recipe.category}</span>
-						</p>
-					</div>
-				</div>
-			</Link>
-		);
-	});
-
 	return (
-		<>
-			<div className='recipeList__container'>{recipe}</div>
-		</>
+		<div className='recipeList__container'>
+			{recipeList.map(recipe => {
+				return (
+					<Link to={`/recipes/${recipe.id}`} key={recipe.id}>
+						<RecipeCard recipe={recipe}/>
+					</Link>
+				);
+			})}
+		</div>
 	);
 };
 
